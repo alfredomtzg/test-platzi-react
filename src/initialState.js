48a512fd90ae7b2e1768a983468ba9cbc0ccab6d
Alfredo Martinez Garcia <psialfredomtz@gmail.com>
2021-02-23 23:11:45 -0600

    update description

--- src/initialState.js
+++ src/initialState.js
@@ -6,7 +6,7 @@ export default {
       'image': 'https://arepa.s3.amazonaws.com/camiseta.png',
       'title': 'Camiseta',
       'price': 25,
-      'description': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+      'description': 'Lore ipsum dolor sit amet, consectetur adipiscing elit',
     },
     {
       'id': '3',
