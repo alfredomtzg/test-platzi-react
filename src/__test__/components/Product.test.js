a0e7fa0fa58095e4b7e3f0eb0041480bddd5861a
Alfredo Martinez Garcia <psialfredomtz@gmail.com>
2021-03-03 22:06:10 -0600

    [update] test click and product component

---
+++ src/__test__/components/Product.test.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { mount, shallow } from 'enzyme';
+import ProviderMock from '../../__mocks__/ProviderMock';
+import ProductMock from '../../__mocks__/ProductMock';
+import Product from '../../components/Product';
+
+describe('<Product/>', () => {
+  test('should be have a render Product', () => {
+    const product = shallow(
+      <ProviderMock>
+        <Product />
+      </ProviderMock>,
+    );
+    expect(product.length).toEqual(1);
+  });
+
+  test('should be have a click shop', () => {
+    const handleAddToCart = jest.fn();
+    const wrapper = mount(
+      <ProviderMock>
+        <Product
+          product={ProductMock}
+          handleAddToCart={handleAddToCart}
+        />
+      </ProviderMock>,
+    );
+    wrapper.find('button').simulate('click');
+    expect(handleAddToCart).toHaveBeenCalledTimes(1);
+  });
+
+});
+
